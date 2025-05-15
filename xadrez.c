f8d5a27cb577f8a73b202d9146e3c44b8ae6a7ce
Bryan.dev <bryanmiranda.dev@gmail.com>
2025-05-15 00:28:18 +0000

    bispo torre e rainha

--- xadrez.c
+++ xadrez.c
@@ -4,19 +4,37 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main()
+{
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+
+    int torre = 0, bispo = 0, rainha;
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    do
+    {
+        printf("(Bispo) - Cima, Direita\n");
+
+        bispo++;
+    } while (bispo < 5);
+    printf("\n");
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    while (torre < 5)
+    {
+        printf("(Torre) - Direita\n");
+
+        torre++;
+    }
+    printf("\n");
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    for (rainha = 0; rainha < 8; rainha++)
+    {
+        printf("(Rainha) - Esquerda\n");
+    }
 
+    
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
